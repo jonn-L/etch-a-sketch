6fa66183629232476319de904aae37b711686618
Jon Lumi <jlumi@constructor.university>
2023-06-12 16:37:17 +0200

    add darkening effect after multiple interactions with a square

--- main.js
+++ main.js
@@ -15,13 +15,37 @@ function create_grid(user_input) {
 
 function attachEventListeners() {
     const squares = document.querySelectorAll('.grid-square');
-    squares.forEach(square => square.addEventListener('mouseover', function(e) {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
+    squares.forEach(square => {
+        let interactions = 0;
+        let currentColor = null;
+        
+        square.addEventListener('mouseover', function(e) {
+            
+            if (interactions == 0) {
+                let r = Math.floor(Math.random() * 256);
+                let g = Math.floor(Math.random() * 256);
+                let b = Math.floor(Math.random() * 256);
 
-        e.target.setAttribute('style', `background-color:rgb(${r},${g}, ${b})`);
-    }));
+                currentColor = [r,g,b];
+                e.target.style.backgroundColor = 
+                    `rgb(${currentColor[0]},${currentColor[1]}, ${currentColor[2]})`;
+            }
+            else {
+                let darkenPercentage = interactions * 10;
+                currentColor[0] = Math.floor(currentColor[0]*
+                                            (1 - darkenPercentage/100));
+                currentColor[1] = Math.floor(currentColor[1]*
+                                            (1 - darkenPercentage/100));
+                currentColor[2] = Math.floor(currentColor[2]*
+                                            (1 - darkenPercentage/100));
+
+                e.target.style.backgroundColor = 
+                    `rgb(${currentColor[0]},${currentColor[1]}, ${currentColor[2]})`;
+            }
+
+            interactions++;
+        })
+    });
 }
 
 create_grid(16);
